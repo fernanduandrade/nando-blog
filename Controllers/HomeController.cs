c03b421d5b4f4ad5e8c118e0e7146d806b79c2ed
fernanduandrade <onanduandrade@gmail.com>
2021-08-10 21:56:33 -0300

    Adicionado conexao com banco & docker

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,17 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using my_blog.Data;
 using my_blog.Models;
 
 namespace my_blog.Controllers
 {
     public class HomeController : Controller
     {
+        private DataContext _ctx;
+        public HomeController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,8 +29,10 @@ namespace my_blog.Controllers
         }
 
         [HttpPost]
-        public IActionResult Edit(Post post)
+        public async Task<IActionResult> Edit(Post post)
         {
+            _ctx.Add(post);
+            await _ctx.SaveChangesAsync();
             return RedirectToAction("Index");
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -1,22 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using my_blog.Data;
 
 namespace my_blog
 {
     public class Startup
     {
+        private IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<DataContext>(options => options.UseNpgsql(_config.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
