6068efb229a809a3d8c159460b744c0a31bcc89b
fernanduandrade <onanduandrade@gmail.com>
2021-08-19 19:59:02 -0300

    um pouco de css

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace my_blog.ViewModels
 {
     public class LoginViewModel
     {
         public string UserName { get; set; }
+        
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
