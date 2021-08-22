a6a131083508b00833bcaa2fdde0eea2e455254d
fernanduandrade <nando.jua@gmail.com>
2021-08-22 14:11:53 -0300

    adicionado solução do projeto

--- Startup.cs
+++ Startup.cs
@@ -39,10 +39,10 @@ namespace my_blog
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //}
+            //    app.UseDeveloperExceptionPage();
 
             app.UseStaticFiles();
 
